d8911541d8c2591d6239447d374bc4dbb5a2072a
Alper Tunga <alper.tunga@yazilim.academy>
2025-06-03 21:49:50 +0300

    Add games and distributors tables

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -1,8 +1,23 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const gamesTable = pgTable("games", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text(),
+  logo: varchar({ length: 255 }),
+});
+
+export const distributorsTable = pgTable("distributors", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text(),
+  website: varchar({ length: 255 }),
+  logo: varchar({ length: 255 }),
 });
