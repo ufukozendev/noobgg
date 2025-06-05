bfc6f09f535ec4bafc0c3a17b5b379fa13eba9b8
Furkan Özay <furkanczay@gmail.com>
2025-06-05 12:54:56 +0300

    feat: defined template files for sidebar implementation

---
+++ apps/web/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import { DashboardSidebar } from "@/components/dashboard/layout/sidebar";
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <SidebarInset>
+        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
